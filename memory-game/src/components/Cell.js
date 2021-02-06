97286719db66afb0a2922c28041f52e4d46650fa
DarMatt <daryamatviienko@gmail.com>
2021-02-06 15:13:17 +0200

    40%

---
+++ memory-game/src/components/Cell.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export let Status = {
+  Open: "Open",
+  Closed: "Closed",
+  Done: "Done",
+  Failed: "Failed",
+}
+
+export let isOpen = (cell) => cell.status === Status.Open
+
+export let isClosed = (cell) => cell.status === Status.Closed
+
+export let isDone = (cell) => cell.status === Status.Done
+
+export let isFailed = (cell) => cell.status === Status.Failed
+
+export let isBlocking = (cell) => isOpen(cell) || isFailed(cell)
+
+export function View({cell, onClick}) {
+  let {status, symbol} = cell
+  return <div className={`cell ${classByStatus(status)}`} onClick={onClick}>
+    {status === Status.Closed ? "" : symbol}
+  </div>
+}
+
+export function classByStatus(status) {
+  return status.toLowerCase()
+}
